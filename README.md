231a7685259bd6d28bbea617cb5d496aa4aad30e
Mridul Dhiman <dhimanmridul91@gmail.com>
2025-03-04 22:05:45 +0530

    made some changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-### User feed Change Data Capture
+### User feed using Change Data Capture and  KFC(Kafka, Flink, Cassandra) Stack
 
 ### Local Development
 #### Cassandra DB
@@ -8,25 +8,6 @@
 #!/bin/bash
 docker network create cassandra
 docker run --rm -d --name cassandra -p 9042:9042 --hostname cassandra --network cassandra cassandra
-docker exec -it cassandra cqlsh
-
-### create new keyspace
-CREATE KEYSPACE IF NOT EXISTS user_activity 
-WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-
-USE user_activity;
-
-### create new table
-CREATE TABLE IF NOT EXISTS user_feed (
-    user_id UUID,
-    activity_id TIMEUUID,
-    activity_type TEXT,
-    timestamp TIMESTAMP,
-    target_id UUID,
-    target_type TEXT,
-    metadata MAP<TEXT, TEXT>,
-    PRIMARY KEY ((user_id), activity_id)
-) WITH CLUSTERING ORDER BY (activity_id DESC);
 ```
 
 #### Local server
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from fastapi import FastAPI
 from typing import Union
-
 from cassandra.cluster import Cluster 
+import os
 
 cluster = Cluster()
-session = cluster.connect('user_activity')
 
-app = FastAPI()
+app = FastAPI(title="user-feed")
+
+CASSANDRA_KEYSPACE = os.environ.get("CASSANDRA_KEYSPACE")
+CASSANDRA_TABLE = os.environ.get("CASSANDRA_TABLE")
+
+
+## Connect to cassandra
+def get_cassandra_session():
+    cluster = Cluster()
+    session = cluster.connect()
+    
+    ### Create Keyspace
+    session.execute(f"""
+        CREATE KEYSPACE IF NOT EXISTS {CASSANDRA_KEYSPACE} 
+        WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+    """)
+    ### create table
+    session.execute(f"""
+    CREATE TABLE IF NOT EXISTS {CASSANDRA_KEYSPACE}.{CASSANDRA_TABLE} (
+    user_id UUID,
+    activity_id TIMEUUID,
+    activity_type TEXT,
+    timestamp TIMESTAMP,
+    target_id UUID,
+    target_type TEXT,
+    metadata MAP<TEXT, TEXT>,
+    PRIMARY KEY ((user_id), activity_id))
+    WITH CLUSTERING ORDER BY (activity_id DESC);
+    """)
+    return session
+
+
+    
 
 @app.get("/")
 def read_root():
